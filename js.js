da3cd5a608a1bef76bc160516e1d58aad5be6464
SUPER-COOL <70461088+SUPER-COOL@users.noreply.github.com>
2020-11-13 10:57:37 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,38 +1,33 @@
 (function () {
   const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-  let birthday = "Nov 13, 2020 10:57:00",
-    countDown = new Date(birthday).getTime(),
-    x = setInterval(function () {
-      let now = new Date().getTime(),
-        distance = countDown - now;
+  let birthday = "Sep 30, 2021 00:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
 
-      (document.getElementById("days").innerText = Math.floor(distance / day)),
-        (document.getElementById("hours").innerText = Math.floor(
-          (distance % day) / hour
-        )),
-        (document.getElementById("minutes").innerText = Math.floor(
-          (distance % hour) / minute
-        )),
-        (document.getElementById("seconds").innerText = Math.floor(
-          (distance % minute) / second
-        ));
+        let now = new Date().getTime(),
+            distance = countDown - now;
 
-      //do something later when date is reached
-      if (distance < 0) {
-        let headline = document.getElementById("headline"),
-          countdown = document.getElementById("countdown"),
-          content = document.getElementById("content");
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-        headline.innerText = "It's my birthday!";
-        countdown.style.display = "none";
-        content.style.display = "block";
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
 
-        clearInterval(x);
-      }
-      //seconds
-    }, 0);
-})();
+          headline.innerText = "It's my birthday!";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
